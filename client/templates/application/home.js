1ef00ac0ec4464836aebf650c1ac2acf303499b0
Ramón Serrano <ramon.calle.88@gmail.com>
2015-04-25 20:51:23 -0430

    Removed animate link.

--- client/templates/application/home.js
+++ client/templates/application/home.js
@@ -5,63 +5,62 @@ var scripts = '<script src="/js/plugins/jquery-scrolltofixed.js"></script>\
     <script src="/js/plugins/classie.js"></script>';
 
 Template.home.rendered = function() {
-	$('body').append(scripts);
+    $('body').append(scripts);
 
-	$('#nav-scroll').scrollToFixed();
-	$('.res-nav_click').click(function() {
+    $('#nav-scroll').scrollToFixed();
+    $('.res-nav_click').click(function() {
         $('.main-nav').slideToggle();
-        return false    
+        return false;
     });
 
-    $('.main-nav li a').bind('click',function(event){
-		var $anchor = $(this);
-		
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top - 102
-		}, 1500,'easeInOutExpo');
-		/*
-		if you don't want to use the easing effects:
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1000);
-		*/
-		event.preventDefault();
-	});
+    $('.main-nav li a').bind('click', function(event) {
+        var $anchor = $(this);
 
-	// Portfolio Conf
-	var $portfolioContainer = $('.portfolioContainer'),
-        $body = $('body'),
-        colW = 375,
-        columns = null;
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 102
+        }, 1500, 'easeInOutExpo');
+        /*
+         if you don't want to use the easing effects:
+         $('html, body').stop().animate({
+         scrollTop: $($anchor.attr('href')).offset().top
+         }, 1000);
+         */
+        event.preventDefault();
+    });
 
-  	$portfolioContainer.isotope({
-		// disable window resizing
-		resizable: true,
-		masonry: {
-		  columnWidth: colW
-		}
-	});
-    
-  	$(window).smartresize(function() {
-        // check if columns has changed
-        var currentColumns = Math.floor( ( $body.width() -30 ) / colW );
-        if ( currentColumns !== columns ) {
-	        // set new column count
-	        columns = currentColumns;
-	        // apply width to container manually, then trigger relayout
-	        $portfolioContainer.width( columns * colW ).isotope('reLayout');
+    // Portfolio Conf
+    var $portfolioContainer = $('.portfolioContainer'),
+            $body = $('body'),
+            colW = 375,
+            columns = null;
+
+    $portfolioContainer.isotope({
+        // disable window resizing
+        resizable: true,
+        masonry: {
+            columnWidth: colW
         }
-  	}).smartresize(); // trigger resize to set container width
+    });
 
-  	// Portfolio filter
-  	$('.portfolioFilter a').click(function(){
+    $(window).smartresize(function() {
+        // check if columns has changed
+        var currentColumns = Math.floor(($body.width() - 30) / colW);
+        if (currentColumns !== columns) {
+            // set new column count
+            columns = currentColumns;
+            // apply width to container manually, then trigger relayout
+            $portfolioContainer.width(columns * colW).isotope('reLayout');
+        }
+    }).smartresize(); // trigger resize to set container width
+
+    // Portfolio filter
+    $('.portfolioFilter a').click(function() {
         $('.portfolioFilter .current').removeClass('current');
         $(this).addClass('current');
- 
+
         var selector = $(this).attr('data-filter');
         $portfolioContainer.isotope({
-			
-            filter: selector,
+            filter: selector
         });
         return false;
     });
